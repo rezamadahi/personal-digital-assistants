4542fb5e6b08db237686bed8f9771e6e2d1f5819
Reza Madahi <reza.maddahi@outlook.com>
2024-12-08 00:57:46 +0330

    complete the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,74 @@
 import ProfileCard from "./ProfileCard";
+import {useState, useEffect} from 'react';
 
+import './assets/css/styles.css';
+import AlexaImage from './assets/images/alexa.png';
+import CortanaImage from './assets/images/cortana.png';
+import SiriImage from './assets/images/siri.png';
 
 function App() {
-    return (
-        <div>
-            <div>Personal Digital Assistants</div>
 
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+    const [darkMode, setDarkMode] = useState(false);
+    useEffect(() => {
+        document.body.className = darkMode ? 'dark-mode' : 'light-mode';
+    }, [darkMode]);
+
+    return (
+        <div className={ darkMode ? 'theme-dark' : 'theme-light' }>
+
+            <div>
+                <nav className="navbar">
+                    <div className="navbar-brand">
+                        <span className="navbar-item">Theme Switcher</span>
+                    </div>
+                    <div className="navbar-end">
+                        <button className="button is-primary" onClick={ () => setDarkMode(!darkMode) }>
+                            { darkMode ? 'Light mode' : 'Dark mode'}
+                        </button>
+                    </div>
+                </nav>
+                <section className="section">
+                    <h1 className="title">Welcome to My App</h1>
+                    <p className="subtitle">Dark Mode is {darkMode ? 'On' : 'Off'}</p>
+                </section>
+            </div>
+
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-3">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa99"
+                                description="Alexa was created by Amazon and helps you buy things."
+                                image={AlexaImage}
+                                imageAlt="Alexa"/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana32"
+                                description="Cortana was made by Microsoft. who knows what it does?"
+                                image={CortanaImage}
+                                imageAlt="Cortana"/>
+                        </div>
+                        <div className="column is-3">
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri01"
+                                description="Siri was made by Apple and is being phase out."
+                                image={SiriImage}
+                                imageAlt="Siri"/>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,27 @@
+function ProfileCard(props) {
 
+    // function ProfileCard( { title, handle} )  #Solution 3
 
-function ProfileCard() {
-    return <div>Profile Card</div>
+    // const title = props.title; #Solution 1
+    // const handle = props.handle;
+
+    const { title , handle, image, imageAlt, description } = props;
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={imageAlt} />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{ title }</p>
+                    <p className="subtitle is-6">{ handle }</p>
+                </div>
+                <div className="content">{ description }</div>
+            </div>
+        </div>
+    );
 }
 
 export default ProfileCard;
